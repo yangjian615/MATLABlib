8f5d9891e66531ec3eeb1b2b253d254ffa2204d8
Matthew Argall <matthew.argall@wildcats.unh.edu>
2016-08-30 14:52:06 -0400

    FIX: fft_n_max- assert that inputs are floats; required for math.

--- fft_utils/fft_n_max.m
+++ fft_utils/fft_n_max.m
@@ -7,6 +7,11 @@ function [n_max] = merge_n_max(t, clen, n_shift)
     if nargin < 3
         n_shift = clen/4;
     end
+    
+    % Integer math rounds. It does not truncate. CLEN and N_SHIFT
+    % must be singles or doubles
+    assert( ~isa(clen,    'int'), 'CLEN must be floating point number.')
+    assert( ~isa(n_shift, 'int'), 'N_SHIFT must be floating point number.')
 
     n_max = floor( (length(t) - clen) / n_shift) + 1;
 end
